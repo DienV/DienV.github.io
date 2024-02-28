ce59c8ffebebd0345fef6cc0b90befe2062d3714
DienV <90385916+DienV@users.noreply.github.com>
2024-02-28 10:58:55 +0700

    Delete New folder directory

--- New folder/scripts.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Kiểm tra thông tin  đăng nhập
-});
-
-document.getElementById('register-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Kiểm tra thông tin  đăng ký
-});
