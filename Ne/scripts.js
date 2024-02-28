968b535bf2587d9bad07ed14ea9d1d96f0872726
DienV <90385916+DienV@users.noreply.github.com>
2024-02-28 11:01:27 +0700

    Add files via upload

---
+++ Ne/scripts.js
@@ -0,0 +1,9 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Kiểm tra thông tin  đăng nhập
+});
+
+document.getElementById('register-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Kiểm tra thông tin  đăng ký
+});
