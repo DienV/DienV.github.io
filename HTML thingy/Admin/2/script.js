99f3c2542b185a1374b4f1f5a74a2bf060d6a7a3
DienV <90385916+DienV@users.noreply.github.com>
2024-03-04 18:37:33 +0700

    Update script.js

--- HTML thingy/Admin/2/script.js
+++ HTML thingy/Admin/2/script.js
@@ -21,46 +21,24 @@ document.addEventListener('DOMContentLoaded', function() {
     averageScore: 75
   };
 
-  // Function to display exam list
-  function displayExamList() {
-    examList.innerHTML = `<h3>Exam List:</h3>`;
-    examData.forEach(exam => {
-      examList.innerHTML += `<p>${exam.name} - ${exam.status}</p>`;
-    });
-  }
-
-  // Function to display user list
-  function displayUserList() {
-    userList.innerHTML = `<h3>User List:</h3>`;
-    userData.forEach(user => {
-      userList.innerHTML += `<p>${user.username} - ${user.email}</p>`;
-    });
-  }
-
-  // Function to display statistics
-  function displayStatistics() {
-    statistics.innerHTML = `
-      <h3>Statistics:</h3>
-      <p>Total Users: ${statisticsData.totalUsers}</p>
-      <p>Total Exams: ${statisticsData.totalExams}</p>
-      <p>Completion Rate: ${statisticsData.completionRate}</p>
-      <p>Average Score: ${statisticsData.averageScore}</p>
-    `;
-  }
-
-  // Display exam list by default
-  displayExamList();
-
-  // Event listeners for options
-  examList.addEventListener('click', function() {
-    displayExamList();
+  // Display exam list
+  examList.innerHTML = `<h3>Exam List:</h3>`;
+  examData.forEach(exam => {
+    examList.innerHTML += `<p>${exam.name} - ${exam.status}</p>`;
   });
 
-  userList.addEventListener('click', function() {
-    displayUserList();
+  // Display user list
+  userList.innerHTML = `<h3>User List:</h3>`;
+  userData.forEach(user => {
+    userList.innerHTML += `<p>${user.username} - ${user.email}</p>`;
   });
 
-  statistics.addEventListener('click', function() {
-    displayStatistics();
-  });
+  // Display statistics
+  statistics.innerHTML = `
+    <h3>Statistics:</h3>
+    <p>Total Users: ${statisticsData.totalUsers}</p>
+    <p>Total Exams: ${statisticsData.totalExams}</p>
+    <p>Completion Rate: ${statisticsData.completionRate}</p>
+    <p>Average Score: ${statisticsData.averageScore}</p>
+  `;
 });
