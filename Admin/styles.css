eebf3768ae866e3ad8f07f54174cb33fd65321ab
DienV <90385916+DienV@users.noreply.github.com>
2024-03-04 17:04:54 +0700

    Add files via upload

---
+++ Admin/styles.css
@@ -0,0 +1,27 @@
+.container {
+  max-width: 800px;
+  margin: 50px auto;
+}
+
+h1 {
+  margin-bottom: 20px;
+}
+
+.tabs {
+  margin-bottom: 20px;
+}
+
+.tab {
+  background-color: #f9f9f9;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+.tabContent {
+  display: none;
+}
+
+.tabContent.active {
+  display: block;
+}
